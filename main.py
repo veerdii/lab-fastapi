acdb0d65ae687b465eeb1705aa1fc35f6bc803fd
unknown <ksmykla@student.agh.edu.pl>
2023-05-17 09:28:55 +0200

    test

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+app = FastAPI()
+@app.get("/")
+async def root():
+    
+    return {"message": "Hello World"}
+
+
+class StudentCreateSchema(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
